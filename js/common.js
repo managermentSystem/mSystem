6c47251416fc0558adb6528ea298a05712aca527
xuruitao <919950909@qq.com>
2016-09-25 14:21:30 +0800

    20160925

--- js/common.js
+++ js/common.js
@@ -3,7 +3,7 @@ document.addEventListener("touchstart", function (e) {
     startX = e.touches[0].screenX;
     startY = e.touches[0].screenY;
     // console.log("x:" + startX + " y: " + startY);
-}, false);
+}, false)
 
 document.addEventListener("touchend", function (e) {
     // console.log(e);
@@ -25,7 +25,7 @@ document.addEventListener("touchend", function (e) {
         case 4:
             console.log("向左滑");
     }
-}, false);
+}, false)
 
 // 隐藏菜单栏
 function hideMenuNav() {
@@ -66,9 +66,116 @@ function getDirection(startX, startY, endX, endY) {
         case ((angle >= 135 && angle < 180) || (angle >= -180 && angle < -135)):
             return 4;
     }
-};
+}
 
 // 返回角度
 function getAngle(x, y) {
     return Math.atan2(y, x) * (180 / Math.PI);
 }
+
+
+//绑定菜单点击事件
+$("#navMenu").click(function() {
+    showSideBox();
+})
+
+$("#menuBack").click(function() {
+    hideSideBox();
+})
+
+$(".nav-bg").click(function() {
+    hideSideBox();
+})
+
+function showSideBox() {
+    $(".nav-side-box").animate({
+        left: '0'
+    });
+    if (!$(".nav-bg").hasClass("nav-show")) {
+        $(".nav-bg").addClass("nav-show");
+    }
+    // document.body.addEventListener('touchmove', function (event) {
+    //     event.preventDefault();
+    // }, false);
+}
+
+function hideSideBox() {
+    $(".nav-side-box").animate({
+        left: '-65%'
+    });
+    if ($(".nav-bg").hasClass("nav-show")) {
+        $(".nav-bg").removeClass("nav-show");
+    }
+}
+
+
+Zepto(function($){
+    var keys = { 32: 1, 37: 1, 38: 1, 39: 1, 40: 1 };
+
+    function preventDefault(e){
+        e = e || window.event;
+        e.preventDefault && e.preventDefault();
+        e.returnValue = false;
+    }
+
+    function preventDefaultForScrollKeys(e){
+        if(keys[e.keyCode]){
+            preventDefault(e);
+            return false;
+        }
+    }
+
+    // 记录原来的事件函数，以便恢复
+    var oldonwheel, oldonmousewheel1, oldonmousewheel2, oldontouchmove, oldonkeydown;
+    var isDisabled;
+
+    var disableScroll = function(){
+        if(window.addEventListener){ // older FF
+            window.addEventListener('DOMMouseScroll', preventDefault, false);
+        }
+
+        oldonwheel = window.onwheel;
+        window.onwheel = preventDefault; // modern standard
+
+        oldonmousewheel1 = window.onmousewheel;
+        window.onmousewheel = preventDefault; // older browsers, IE
+        oldonmousewheel2 = document.onmousewheel;
+        document.onmousewheel = preventDefault; // older browsers, IE
+
+        oldontouchmove = window.ontouchmove;
+        window.ontouchmove = preventDefault; // mobile
+
+        oldonkeydown = document.onkeydown;
+        document.onkeydown = preventDefaultForScrollKeys;
+        isDisabled = true;
+    };
+
+    var enableScroll = function(){
+        if(!isDisabled){
+            return;
+        }
+        if(window.removeEventListener){
+            window.removeEventListener('DOMMouseScroll', preventDefault, false);
+        }
+
+        window.onwheel = oldonwheel; // modern standard
+
+        window.onmousewheel = oldonmousewheel1; // older browsers, IE
+        document.onmousewheel = oldonmousewheel2; // older browsers, IE
+
+        window.ontouchmove = oldontouchmove; // mobile
+
+        document.onkeydown = oldonkeydown;
+        isDisabled = false;
+    };
+
+    // bind
+    $('#closePopup').on('click', function(e){
+        $('#popupLayer').hide();
+        $('#bgMask').hide();
+        enableScroll();
+    });
+
+    disableScroll();
+
+});
